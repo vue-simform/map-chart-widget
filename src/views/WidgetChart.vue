120532be110d13e11d10800b76b95560776543fb
Moksha-S <moksha@simformsolutions.com>
2024-06-19 11:20:18 +0530

    Chart added as widget

---
+++ src/views/WidgetChart.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <iframe :src="iframeSrc" width="100%" height="600px" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const iframeSrc = ref('http://localhost:5174/')
+</script>
